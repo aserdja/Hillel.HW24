ce1e0a56a4093d8697534e67bde32cbd90a734a0
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-21 16:03:00 +0300

    Created models and DbContext-class. Made CodeFirst

---
+++ HW24/HW24.DAL/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HW24.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HW24.DAL.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public virtual DbSet<User> Users => Set<User>();
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer();
+		}
+	}
+}
---
+++ HW24/HW24.DAL/Migrations/20241021130122_InitialCommit.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HW24.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Biography = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
